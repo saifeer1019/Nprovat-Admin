074fc3297865071d0d6b81417199a07563493fed
Saifee Rahman Saif <saifeer1019@gmail.com>
2025-02-17 13:29:16 +0600

    edits

--- src/app/(DashboardLayout)/util/mongodb.ts
+++ src/app/(DashboardLayout)/util/mongodb.ts
@@ -7,7 +7,7 @@ const connectionToDatabase = async () => {
         if (!MONGODB_URI) {
             throw new Error('MONGODB_URI is not defined');
         }
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(MONGODB_URI as string);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
