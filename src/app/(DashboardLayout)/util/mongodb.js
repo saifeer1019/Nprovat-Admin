2d91773a4c80de9233e213f33cd80027e673567b
Saifee Rahman Saif <saifeer1019@gmail.com>
2025-02-17 13:11:39 +0600

    edits

--- src/app/(DashboardLayout)/util/mongodb.js
+++ src/app/(DashboardLayout)/util/mongodb.js
@@ -4,6 +4,9 @@ const MONGODB_URI = process.env.NEXT_PUBLIC_MONGO_URI;
 
 const connectionToDatabase = async () => {
     try {
+        if (!MONGODB_URI) {
+            throw new Error('MONGODB_URI is not defined');
+        }
         await mongoose.connect(MONGODB_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
