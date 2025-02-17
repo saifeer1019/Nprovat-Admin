68f5e84d5eb4574cb59f6e9f4009f2da7f241a50
Saifee Rahman Saif <saifeer1019@gmail.com>
2025-02-17 13:32:28 +0600

    edits

--- src/utils/mongodb.tsx
+++ src/utils/mongodb.tsx
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.NEXT_PUBLIC_MONGO_URI;
 
+if (!MONGODB_URI) {
+    throw new Error('Please define the NEXT_PUBLIC_MONGO_URI environment variable inside .env.local');
+}
+
 const connectionToDatabase = async () => {
     try {
         await mongoose.connect(MONGODB_URI);
